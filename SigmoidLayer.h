c922159ce3558b9ba7d53a7f45c01192425d35e5
Jiahui Lu <lujiahui1@hotmail.com>
2022-03-30 16:11:33 -0400

    Update SigmoidLayer.h

--- SigmoidLayer.h
+++ SigmoidLayer.h
@@ -16,7 +16,7 @@ private:
 	SigmoidConfig conf;
     // the output of this layer
 	RSSVectorMyType activations; 
-    // the delta of input
+    // the delta of output
 	RSSVectorMyType deltas; 
 
 public:
@@ -28,8 +28,8 @@ public:
     // inputActivation is the input of this layer, i.e. wtx
     // The output of this layer (activations) is approx of adjusted sigmoid(wtx) = approx of 2/(1+exp(wtx))-1 = 0.5 * wtx
 	void forward(const RSSVectorMyType& inputActivation) override; 
-    // prevDelta is the delta of output
-    // delta = 0.25 * wtx - 0.5 * y = 0.5 * activations - 0.5 * (activations - prevDelta) = 0.5 * prevDelta
+    // prevDelta is the delta of input
+    // prevDelta = 0.25 * wtx - 0.5 * y = 0.5 * activations - 0.5 * (activations - deltas) = 0.5 * deltas
 	void computeDelta(RSSVectorMyType& prevDelta) override; 
 	void updateEquations(const RSSVectorMyType& prevActivations) override;
 
